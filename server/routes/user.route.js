a2ac42bff1a96cfa9e25869cd1bc79d0a2be1570
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-04-22 11:33:59 +0100

    feature(endpoint for credit account):write unit test and build the endpoint [Finishes  #165491027]

--- server/routes/user.route.js
+++
@@ -1,9 +0,0 @@
-// import express from 'express';
-// import UserController from '../controllers/user.controller';
-
-// const userRoute = express.Router();
-
-// userRoute.post('/signup', UserController.register);
-// userRoute.post('/signin', UserController.signIn);
-
-// export default userRoute;
--- server/usingDB/route/transactRouteWithDB.js
+++ server/usingDB/route/transactRouteWithDB.js
@@ -6,5 +6,7 @@ import { transactionValidation } from '../middleware/transactValidation'
 const transactionRoute = express.Router();
 
 transactionRoute.post('/:accountNumber/debit', transactionValidation, authorize.checkCashier, transactionController.debitAccount);
+transactionRoute.post('/:accountNumber/credit', transactionValidation, authorize.checkCashier, transactionController.creditAccount);
+
 
 export default transactionRoute;
