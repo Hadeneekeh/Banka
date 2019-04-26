2db8e9793928dccdbbf04e753d850a2244ab8169
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-04-26 04:18:14 +0100

    feature(create users): add Admin can ussers endpoint [Delivers #165626027]

--- server/usingDB/route/userRouteWithDB.js
+++ server/usingDB/route/userRouteWithDB.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import dbUserController from '../controllers/user-controller';
-import { signUpValidation, signInValidation } from '../middleware/userValidation';
+import { signUpValidation, signInValidation, createStafValidation } from '../middleware/userValidation';
+import authorize from '../middleware/userAuthorization';
 
 const dbUserRoute = express.Router();
 
 dbUserRoute.post('/signup', signUpValidation, dbUserController.signupUser);
 dbUserRoute.post('/signin', signInValidation, dbUserController.signinUser);
+dbUserRoute.post('/create', authorize.checkAdmin, createStafValidation, dbUserController.createStaff);
 
 export default dbUserRoute;
