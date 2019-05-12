2c80e6ff208714ee630e67bc1b92012de3a932df
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-05-12 21:19:16 +0100

    feat(display account): -Create an endpoint to display logged in user account details -Write unit test for the endpoint -Fetch the endpoint to display logged in user account details [Delivers  #165954758]

--- UI/script/clientProfile.js
+++ UI/script/clientProfile.js
@@ -1,12 +1,53 @@
+/* eslint-disable prefer-destructuring */
 const profileDetails = JSON.parse(localStorage.getItem('userInfo'));
+const token = JSON.parse(localStorage.getItem('token'));
 
 const userName = document.getElementById('username');
 const userEmail = document.getElementById('useremail');
+const acctOverview = document.getElementById('acctOverview');
+const msgContainer = document.getElementById('errorContainer');
+
 
 const getProfileDettails = () => {
   userName.innerText = `${profileDetails.firstName}, ${profileDetails.lastName}`;
   userEmail.innerText = profileDetails.email;
 };
 
+const showAcctOverview = () => {
+  const emailAddress = profileDetails.email;
+
+  fetch(`https://banka-adc.herokuapp.com/api/v1/user/accounts?email=${emailAddress}`, {
+    method: 'GET',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then((response) => {
+      let acctDisp = '';
+      if (response.status === 200) {
+        response.accounts.forEach((account) => {
+          acctDisp += `
+            <div class="acctDetails">
+                        <h2>${account.accountnumber}</h2>
+                        <p>Account Type</p>
+                        <h4>${account.type}</h4>
+                        <p>Account Status</p>
+                        <h4>${account.status}</h4>
+                        <p>Account Balance</p>
+                        <h4>₦ ${account.balance}</h4>
+                    </div>`;
+        });
+        acctOverview.innerHTML = acctDisp;
+      }
+    })
+    .catch((error) => {
+      msgContainer.style.backgroundColor = 'white';
+      msgContainer.style.color = 'red';
+      msgContainer.innerHTML = 'Unable to connect' || error.message;
+    });
+};
 
 getProfileDettails();
+showAcctOverview();
--- UI/script/createAccount.js
+++ UI/script/createAccount.js
@@ -3,8 +3,21 @@
 const api = 'https://banka-adc.herokuapp.com/api/v1/accounts';
 
 const createAccount = document.getElementById('createAcct');
-const msgContainer = document.getElementById('errorContainer');
+const errMsgContainer = document.getElementById('errorContainer');
 const createAccountBtn = document.getElementById('createAcct-btn');
+
+
+const userDetails = JSON.parse(localStorage.getItem('userInfo'));
+const userName = document.getElementById('username');
+const userEmail = document.getElementById('useremail');
+
+const getProfileDettails = () => {
+  userName.innerText = `${userDetails.firstName}, ${userDetails.lastName}`;
+  userEmail.innerText = userDetails.email;
+};
+
+
+const token = JSON.parse(localStorage.getItem('token'));
 
 createAccount.addEventListener('submit', (event) => {
   event.preventDefault();
@@ -13,8 +26,6 @@ createAccount.addEventListener('submit', (event) => {
 
   const type = document.getElementById('accountType').value;
 
-  const userDetails = JSON.parse(localStorage.getItem('userInfo'));
-  const token = userDetails.token;
 
   fetch(api, {
     method: 'POST',
@@ -29,30 +40,31 @@ createAccount.addEventListener('submit', (event) => {
     .then(res => res.json())
     .then((response) => {
       if (response.status === 201) {
-        msgContainer.style.backgroundColor = 'white';
-        msgContainer.style.color = 'black';
-        msgContainer.innerHTML = 'Loading...';
+        errMsgContainer.style.backgroundColor = 'white';
+        errMsgContainer.style.color = 'black';
+        errMsgContainer.innerHTML = 'Loading...';
         setTimeout(() => {
           window.location = './client-dashboard.html';
         }, 2000);
-
-        localStorage.setItem('userInfo', JSON.stringify(response.data));
       }
       if (response.status === 400) {
-        msgContainer.style.backgroundColor = 'white';
-        msgContainer.style.color = 'red';
-        msgContainer.style.padding = '1px';
-        msgContainer.style.marginTop = '2px';
-        msgContainer.innerHTML = response.error;
+        errMsgContainer.style.backgroundColor = 'white';
+        errMsgContainer.style.color = 'red';
+        errMsgContainer.style.padding = '1px';
+        errMsgContainer.style.marginTop = '2px';
+        errMsgContainer.innerHTML = response.error;
         createAccountBtn.value = 'Create an Account';
       }
       setTimeout(() => {
-        msgContainer.style.display = 'none';
+        errMsgContainer.style.display = 'none';
       }, 5000);
     })
     .catch((error) => {
-      msgContainer.style.backgroundColor = 'white';
-      msgContainer.style.color = 'red';
-      msgContainer.innerHTML = 'Unable to connect' || error.message;
+      errMsgContainer.style.backgroundColor = 'white';
+      errMsgContainer.style.color = 'red';
+      errMsgContainer.innerHTML = 'Unable to connect' || error.message;
     });
 });
+
+
+getProfileDettails();
